670ee38b476b307d667298d73c3317411baa8975
KyleAngeles <kyle.angeles@dcmail.ca>
2025-09-25 10:58:29 -0400

    #So, Lastly adding comments with the author, desciption, and date, and lastly added intialized button for the buttons to be clickable

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// Author: Kule Angeles
+// Date: 9/24/2025
+// Description: This is a simple Tic Tac Toe game using WPF for the UI. The game allows two players to take turns marking X and O on a 3x3 grid. The game checks for a win or a draw after each move and displays the result. Players can reset the game to play again or exit the application.
+//
 namespace TicTacToeUIGame
 {
     /// <summary>
@@ -16,9 +20,11 @@ namespace TicTacToeUIGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Button> buttons = new List<Button>();
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void ExitBtn_Click(object sender, RoutedEventArgs e)
@@ -30,6 +36,11 @@ namespace TicTacToeUIGame
         private void ResetBtn_Click(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
